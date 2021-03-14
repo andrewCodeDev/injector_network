064ea93d0706afabe6687a98b8157608df45fdbe
andrewCodeDev <andrewmccommons@gmail.com>
2021-03-13 21:34:39 -0800

    activation functions simplified

--- include/sensors.hpp
+++ include/sensors.hpp
@@ -45,7 +45,9 @@ namespace sensor {
   template<std::floating_point num> struct IMDmodifier { num coef{1}, rate{1}, cntr{0}; };
   template<std::floating_point num> struct SEQmodifier { num coef{1}, rate{1}, cntr{0}, coef_upd{0}, rate_upd{0}, cntr_upd{0}; };
 
+
   // sensor base class ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
   template<std::floating_point num, typename modifier> class sensor_base {
 
@@ -73,15 +75,18 @@ namespace sensor {
       std::vector<modifier> data;
 
       friend class logit::immediate<num>;
+      friend class logit::sequential<num>;
   };
+
+
+  // sensor derived types ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
   template <
     std::floating_point num
-  > struct IMDsensor : public sensor_base<num, IMDmodifier<num>>{
+  > class IMDsensor : public sensor_base<num, IMDmodifier<num>>{
 
-    using modifier = IMDmodifier<num>;
-
+    public:
     IMDsensor() = default;
     IMDsensor(const size_t& size){
 
@@ -101,7 +106,7 @@ namespace sensor {
       if( n_drv == 1 ) {
 
         return std::transform_reduce(this->data.cbegin(), this->data.cend(), (num)0, std::plus<num>{},
-          [&x](const modifier& m) 
+          [&x](const auto& m) 
           { 
             num e_p = std::exp(m.rate * (x - m.cntr));
             return -(m.coef * m.rate * e_p) / (((num)1 + e_p) * ((num)1  + e_p));
@@ -111,7 +116,7 @@ namespace sensor {
       if( n_drv == 2 ) {
 
         return std::transform_reduce(this->data.cbegin(), this->data.cend(), (num)0, std::plus<num>{}, 
-          [&x](const modifier& m)
+          [&x](const auto& m)
           { 
             num e_p = std::exp(m.rate * (x - m.cntr));
             return -(m.coef * m.rate * m.rate * e_p * (-e_p + (num)1)) / (((num)1 + e_p) * ((num)1 + e_p) * ((num)1 + e_p));
@@ -125,7 +130,7 @@ namespace sensor {
 
       num one{1}, adj{error * lr}, e_p, denom;
         
-      for( modifier& m : this->data ) {
+      for( auto& m : this->data ) {
         e_p = std::exp(m.rate * (x - m.cntr)); 
         denom = (one + e_p) * (one + e_p);
 
@@ -137,7 +142,7 @@ namespace sensor {
 
     num operator()( const num& x ){
       this->signal = std::transform_reduce(std::execution::seq, this->data.cbegin(), this->data.cend(), static_cast<num>(0), std::plus<num>{},
-        [&x](const modifier& m)
+        [&x](const auto& m)
         {
           return m.coef / (static_cast<num>(1) + std::exp(m.rate * (x - m.cntr)));
         });
@@ -146,113 +151,81 @@ namespace sensor {
 
   };
 
-    template <
+  template <
     std::floating_point num
-  > class SEQsensor {
-
-    private:
-
-      using modifier = SEQmodifier<num>;
-
-      std::vector<modifier> data; 
-      
-      num signal{0};
-
-      friend class logit::sequential<num>;
-
+  > class SEQsensor : public sensor_base<num, SEQmodifier<num>>{
+    
     public:
-      SEQsensor() = default;
-      SEQsensor(const size_t& size) : data(size){
-        for( auto& m : data ){
-          m.coef = rand_gen<num>();
-          m.rate = rand_gen<num>();
-          m.cntr = rand_gen<num>();
-        }
-      };
-      
-      num dy_dx( const num& x, std::size_t n_drv = 1 ){
-
-        assert( 0 < n_drv && n_drv < 3 );
-
-        if( n_drv == 1 ) {
-
-          return std::transform_reduce(data.cbegin(), data.cend(), (num)0, std::plus<num>{},
-            [&x](const modifier& m) 
-            { 
-              num e_p = std::exp(m.rate * (x - m.cntr));
-              return -(m.coef * m.rate * e_p) / (((num)1 + e_p) * ((num)1  + e_p));
-            });
-        }
-        
-        if( n_drv == 2 ) {
-
-          return std::transform_reduce(data.cbegin(), data.cend(), (num)0, std::plus<num>{}, 
-            [&x](const modifier& m)
-            { 
-              num e_p = std::exp(m.rate * (x - m.cntr));
-              return -(m.coef * m.rate * m.rate * e_p * (-e_p + (num)1)) / (((num)1 + e_p) * ((num)1 + e_p) * ((num)1 + e_p));
-            });
-          }
-
-          return 0.0f; // otherwise it complains
-        }
-
-      void calibrate(const num& error, num lr = 0.01 ){
-        for( modifier& m : data ) {
-          m.rate -= m.rate_upd * error * lr;
-          m.cntr -= m.cntr_upd * error * lr;
-          m.coef -= m.coef_upd * error * lr;
-
-          m.rate_upd = 0;
-          m.cntr_upd = 0;
-          m.coef_upd = 0;
-        }
+    SEQsensor() = default;
+    SEQsensor(const size_t& size){
+      this->data.resize(size);
+      for( auto& m : this->data ){
+        m.coef = rand_gen<num>();
+        m.rate = rand_gen<num>();
+        m.cntr = rand_gen<num>();
       }
+    };
+    
+    num dy_dx( const num& x, std::size_t n_drv = 1 ){
 
-      num operator()( const num& x ){
+      assert( 0 < n_drv && n_drv < 3 );
 
-        signal = std::transform_reduce(std::execution::seq, data.cbegin(), data.cend(), static_cast<num>(0), std::plus<num>{},
-          [&x](const modifier& m)
-          {
-            return m.coef / (static_cast<num>(1) + std::exp(m.rate * (x - m.cntr)));
+      if( n_drv == 1 ) {
+
+        return std::transform_reduce(this->data.cbegin(), this->data.cend(), (num)0, std::plus<num>{},
+          [&x](const auto& m) 
+          { 
+            num e_p = std::exp(m.rate * (x - m.cntr));
+            return -(m.coef * m.rate * e_p) / (((num)1 + e_p) * ((num)1  + e_p));
           });
+      }
+      
+      if( n_drv == 2 ) {
 
-        num one{1}, e_p, denom;
-
-        for( modifier& m : data ) {
-          e_p = std::exp(m.rate * (x - m.cntr)); 
-          denom = (one + e_p) * (one + e_p);
-
-          m.rate_upd += ((-m.coef * (x - m.cntr) * e_p ) / denom );
-          m.cntr_upd += (( m.coef * m.rate * e_p ) / denom );
-          m.coef_upd += (one / (one + e_p));
+        return std::transform_reduce(this->data.cbegin(), this->data.cend(), (num)0, std::plus<num>{}, 
+          [&x](const auto& m)
+          { 
+            num e_p = std::exp(m.rate * (x - m.cntr));
+            return -(m.coef * m.rate * m.rate * e_p * (-e_p + (num)1)) / (((num)1 + e_p) * ((num)1 + e_p) * ((num)1 + e_p));
+          });
         }
 
-        return signal;
+        return 0.0f; // otherwise it complains
       }
 
-      const modifier& operator[](const std::size_t& i ) const { return data[i]; }
-            modifier& operator[](const std::size_t& i )       { return data[i]; }
+    void calibrate(const num& error, num lr = 0.01 ){
+      for( auto& m : this->data ) {
+        m.rate -= m.rate_upd * error * lr;
+        m.cntr -= m.cntr_upd * error * lr;
+        m.coef -= m.coef_upd * error * lr;
 
-      SEQsensor& operator++(){ data.emplace_back( modifier{} ); return *this; }
-      SEQsensor& operator--(){ data.pop_back(); return *this; }
+        m.rate_upd = 0;
+        m.cntr_upd = 0;
+        m.coef_upd = 0;
+      }
+    }
 
-      auto begin() const { return data.begin(); }
-      auto begin()       { return data.begin(); }
-      auto end()   const { return data.end(); }
-      auto end()         { return data.end(); }
+    num operator()( const num& x ){
 
-      auto cbegin() const { return data.cbegin(); }
-      auto cend()   const { return data.cend();   }
+      this->signal = std::transform_reduce(std::execution::seq, this->data.cbegin(), this->data.cend(), static_cast<num>(0), std::plus<num>{},
+        [&x](const auto& m)
+        {
+          return m.coef / (static_cast<num>(1) + std::exp(m.rate * (x - m.cntr)));
+        });
 
-      auto rbegin() { return data.rbegin(); }
-      auto rend()   { return data.rend();   }
+      num one{1}, e_p, denom;
 
-      std::size_t size() const { return data.size(); }
+      for( auto& m : this->data ) {
+        e_p = std::exp(m.rate * (x - m.cntr)); 
+        denom = (one + e_p) * (one + e_p);
 
-      // implicit conversions ~~~~~~~~~~~~~~~~~~~~~~~~
-      
-      operator num() const { return signal; }
+        m.rate_upd += ((-m.coef * (x - m.cntr) * e_p ) / denom );
+        m.cntr_upd += (( m.coef * m.rate * e_p ) / denom );
+        m.coef_upd += (one / (one + e_p));
+      }
+
+      return this->signal;
+    }
 
   };
 
